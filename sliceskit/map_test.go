63c1462025e21ca9d02a029360bbad2ce0957750
leozhao0709 <leo.zhao.real@gmail.com>
2025-01-23 10:20:18 -0800

    minor update

--- sliceskit/map_test.go
+++ sliceskit/map_test.go
@@ -11,9 +11,9 @@ type mapSuite struct {
 	suite.Suite
 }
 
-func (s *mapSuite) TestMap() {
+func (s *mapSuite) TestMapFunc() {
 	s.Run("should return nil when input slice is nil", func() {
-		result := Map[[]int](nil, func(e int, _ int) int { return e * 2 })
+		result := Map[[]int](nil, func(e int) int { return e * 2 })
 		assert.Nil(s.T(), result)
 	})
 
@@ -24,8 +24,26 @@ func (s *mapSuite) TestMap() {
 
 	s.Run("should return mapped slice", func() {
 		slice := []int{1, 2, 3}
-		result := Map(slice, func(e int, _ int) int { return e * 2 })
+		result := Map(slice, func(e int) int { return e * 2 })
 		assert.Equal(s.T(), []int{2, 4, 6}, result)
+	})
+}
+
+func (s *mapSuite) TestMapFuncIndex() {
+	s.Run("should return nil when input slice is nil", func() {
+		result := MapWithIndex[[]int](nil, func(e int, i int) int { return e * i })
+		assert.Nil(s.T(), result)
+	})
+
+	s.Run("should return nil when mapFunc is nil", func() {
+		result := MapWithIndex[[]int, int]([]int{1, 2, 3}, nil)
+		assert.Nil(s.T(), result)
+	})
+
+	s.Run("should return mapped slice", func() {
+		slice := []int{1, 2, 3}
+		result := MapWithIndex(slice, func(e int, i int) int { return e * i })
+		assert.Equal(s.T(), []int{0, 2, 6}, result)
 	})
 }
 
