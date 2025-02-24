1dea6ea571b7e9589f4af60a669b94c2697d6733
leozhao0709 <leozhao0709@users.noreply.github.com>
2025-02-23 22:57:23 -0800

    Move generated pb to testdata (#13)

--- scripts/regen_proto.sh
+++ scripts/regen_proto.sh
@@ -9,20 +9,17 @@ for SEARCH_PROTO_FOLDER in "${SEARCH_PROTO_FOLDERS[@]}"; do
     # Define output directories for different languages
     GO_OUT_DIR=$BASE_PATH/$GO_PROJECT/$SEARCH_PROTO_FOLDER/testdata
 
-    rm -rf $GO_OUT_DIR
+    rm -rf $GO_OUT_DIR/*.pb.go
 
     # Create output directories if they don't exist
     mkdir -p $GO_OUT_DIR
 
     # Find all .proto files
-    PROTO_FILES=$(find $BASE_PATH/$SEARCH_PROTO_FOLDER/proto -name "*.proto")
+    PROTO_FILES=$(find $BASE_PATH/$SEARCH_PROTO_FOLDER/testdata/proto -name "*.proto")
 
     # Generate Go files
-    protoc -I $BASE_PATH/$SEARCH_PROTO_FOLDER/proto \
+    protoc -I $BASE_PATH/$SEARCH_PROTO_FOLDER/testdata/proto \
     --go_out=$GO_OUT_DIR --go_opt=paths=source_relative \
     --go-grpc_out=$GO_OUT_DIR --go-grpc_opt=paths=source_relative \
     $PROTO_FILES
 done
-
-
-
