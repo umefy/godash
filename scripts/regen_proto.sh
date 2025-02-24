09c676528289ab47ebfef7226828a31318bf4e8b
leozhao0709 <leo.zhao.real@gmail.com>
2025-02-23 22:49:51 -0800

    move_pb_to_testdata

--- scripts/regen_proto.sh
+++ scripts/regen_proto.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 BASE_PATH="$(cd "$(dirname "$0")/.." && pwd)"
+GO_PROJECT="." # replace with go project
 
-# Define output directories for different languages
-GO_OUT_DIR=$BASE_PATH/internal/testdata
+SEARCH_PROTO_FOLDERS=("jsonkit")
 
-rm -rf $GO_OUT_DIR
+for SEARCH_PROTO_FOLDER in "${SEARCH_PROTO_FOLDERS[@]}"; do
+    # Define output directories for different languages
+    GO_OUT_DIR=$BASE_PATH/$GO_PROJECT/$SEARCH_PROTO_FOLDER/testdata
 
-# Create output directories if they don't exist
-mkdir -p $GO_OUT_DIR
+    rm -rf $GO_OUT_DIR/*.pb.go
 
-# Find all .proto files
-PROTO_FILES=$(find $BASE_PATH/internal/proto -name "*.proto")
+    # Create output directories if they don't exist
+    mkdir -p $GO_OUT_DIR
 
-# Generate Go files
-protoc -I $BASE_PATH/internal/proto \
---go_out=$GO_OUT_DIR --go_opt=paths=source_relative \
---go-grpc_out=$GO_OUT_DIR --go-grpc_opt=paths=source_relative \
-$PROTO_FILES
+    # Find all .proto files
+    PROTO_FILES=$(find $BASE_PATH/$SEARCH_PROTO_FOLDER/testdata/proto -name "*.proto")
 
+    # Generate Go files
+    protoc -I $BASE_PATH/$SEARCH_PROTO_FOLDER/testdata/proto \
+    --go_out=$GO_OUT_DIR --go_opt=paths=source_relative \
+    --go-grpc_out=$GO_OUT_DIR --go-grpc_opt=paths=source_relative \
+    $PROTO_FILES
+done
